67300cacb0dd9197edc8e915877db56bab61d53d
Felix Augustsson <sirfexilus@gmail.com>
2020-11-29 23:42:03 +0100

    Made visualization play animation over frames

--- examples/visualize_data.py
+++ examples/visualize_data.py
@@ -1,8 +1,8 @@
-"""Visualize the data from a specific frame."""
+"""Visualize the data."""
 import numpy as np
 import open3d as o3d
 
-from tracking.data.load import load_point_cloud
+from tracking.data.load import load_point_clouds
 
 
 data_files = ["data/data_109.h5",
@@ -20,17 +20,38 @@ def normalize(vector):
     return vector / np.linalg.norm(vector)
 
 
-pclouds = [load_point_cloud(file).crop(bounding_box) for file in data_files]
+pclouds = [load_point_clouds(file) for file in data_files]
+
+visualizer = o3d.visualization.Visualizer()
+visualizer.create_window(width=800, height=600)
+
+# Add point cloud objects to be used in render
+geometries = [next(pcloud).crop(bounding_box) for pcloud in pclouds]
+for geometry in geometries:
+    visualizer.add_geometry(geometry)
 
 # Set default camera view
-front       = np.asarray((2.0, 4.0, -1.0))
+front       = np.asarray((3.0, 4.0, -1.0))
 lookat      = normalize(np.asarray((0.1, 0.0, 0.0)))
 world_up    = np.asarray((0.0, 0.0, -1.0))
 camera_side = normalize(np.cross(front, world_up))
 camera_up   = np.cross(camera_side, front)
 zoom        = 0.25
 
-o3d.visualization.draw_geometries(pclouds,
-                                  width=800, height=600,
-                                  front=front, lookat=lookat, up=camera_up,
-                                  zoom=zoom)
+view_control = visualizer.get_view_control()
+view_control.set_front(front)
+view_control.set_lookat(lookat)
+view_control.set_up(camera_up)
+view_control.set_zoom(zoom)
+view_control.translate(150, 0)
+
+for i in range(800):
+    # Loop over 800 frames
+    for pcloud, geometry in zip(pclouds, geometries):
+        cur_pcloud = next(pcloud).crop(bounding_box)
+        geometry.points = cur_pcloud.points
+        geometry.colors = cur_pcloud.colors
+        visualizer.update_geometry(geometry)
+
+    visualizer.poll_events()
+    visualizer.update_renderer()
--- tracking/data/load.py
+++ tracking/data/load.py
@@ -12,8 +12,8 @@ seaborn_palette = itertools.cycle(sns.color_palette())
 no_palette = itertools.cycle([(1.0, 1.0, 1.0)])
 
 
-def load_point_cloud(hdf5file, frame=0, palette=seaborn_palette):
-    """Extract a single frame's point cloud from camera data.
+def load_point_clouds(hdf5file, start_frame=0, palette=seaborn_palette):
+    """Generator over point clouds per frame from camera data.
     The point cloud will be colored by the palette to differentiate between
     clouds."""
 
@@ -21,23 +21,28 @@ def load_point_cloud(hdf5file, frame=0, palette=seaborn_palette):
 
     cam2world = np.asarray(camera["/TMatrixCamToWorld"])
 
-    # Transform coordinates to world space
-    cs_points = np.reshape(np.asarray(camera["Sequence"][str(frame)]["Points"]), (-1, 3))
-    cs_points_4 = np.ones((int(np.size(cs_points) / 3), 4))
-    cs_points_4[:,0:3] = cs_points
-    ws_points_4 = (np.matmul(cam2world, (cs_points_4).T)).T
-    ws_points = ws_points_4[:,0:3]
+    frames = itertools.cycle(range(start_frame, len(camera["Sequence"])))
 
-    # Color the points with image data, tinted by color from palette
-    greyscale = np.reshape(np.asarray(camera["Sequence"][str(frame)]["Image"]) / 255,
-                           (-1, 1))
-    colors = np.reshape(np.stack((greyscale, greyscale, greyscale), axis=2),
-                        (-1, 3))
-    colors_painted = colors * np.asarray(next(palette))
+    dataset_color = np.asarray(next(palette))
 
-    # Add data to open3d object
-    pcloud = o3d.geometry.PointCloud()
-    pcloud.points = o3d.utility.Vector3dVector(ws_points)
-    pcloud.colors = o3d.utility.Vector3dVector(colors_painted)
+    for frame in frames:
+        # Transform coordinates to world space
+        cs_points = np.reshape(np.asarray(camera["Sequence"][str(frame)]["Points"]), (-1, 3))
+        cs_points_4 = np.ones((int(np.size(cs_points) / 3), 4))
+        cs_points_4[:,0:3] = cs_points
+        ws_points_4 = (np.matmul(cam2world, (cs_points_4).T)).T
+        ws_points = ws_points_4[:,0:3]
 
-    return pcloud
+        # Color the points with image data, tinted by color from palette
+        greyscale = np.reshape(np.asarray(camera["Sequence"][str(frame)]["Image"]) / 255,
+                            (-1, 1))
+        colors = np.reshape(np.stack((greyscale, greyscale, greyscale), axis=2),
+                            (-1, 3))
+        colors_painted = colors * dataset_color
+
+        # Add data to open3d object
+        pcloud = o3d.geometry.PointCloud()
+        pcloud.points = o3d.utility.Vector3dVector(ws_points)
+        pcloud.colors = o3d.utility.Vector3dVector(colors_painted)
+
+        yield pcloud
