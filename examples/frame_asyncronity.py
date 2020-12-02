a9f243a83f33c7ecce8a198942e75dd736609748
Felix Augustsson <git.fexilus@gmail.com>
2020-12-02 11:04:51 +0100

    Changed asyncronity to compare to expected times

--- examples/frame_asyncronity.py
+++ examples/frame_asyncronity.py
@@ -8,14 +8,29 @@ camera0 = h5py.File("data/data_109.h5", 'r')
 camera1 = h5py.File("data/data_130.h5", 'r')
 
 min_num_frames = min(camera0["Timestamp"].size, camera1["Timestamp"].size)
+
 cropped_times0 = np.asarray(camera0["Timestamp"])[0:min_num_frames]
 cropped_times1 = np.asarray(camera1["Timestamp"])[0:min_num_frames]
 
-time_diff = np.asarray(cropped_times0 - cropped_times1)
+time_step_frames0 = np.diff(np.asarray(camera0["Timestamp"]))
+time_step_frames1 = np.diff(np.asarray(camera1["Timestamp"]))
 
-plt.plot(time_diff)
+average_step_size = np.median(np.concatenate((time_step_frames0,
+                                              time_step_frames1)))
+
+earliest_time = min(camera0["Timestamp"][0], camera1["Timestamp"][0])
+
+expected_end_time = average_step_size * min_num_frames
+expected_times = np.arange(0, expected_end_time, average_step_size)
+
+norm_frame_times0 = cropped_times0 - expected_times - earliest_time
+norm_frame_times1 = cropped_times1 - expected_times - earliest_time
+
+plt.subplot()
+plt.plot(norm_frame_times0)
+plt.plot(norm_frame_times1)
 
 plt.xlabel("Frame number in dataset")
-plt.ylabel("Time difference datasets (s)")
+plt.ylabel("Time difference compared to expected behavior (s)")
 
 plt.show()
