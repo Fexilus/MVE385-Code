b3b917e7c7c091dd25a00e10f1bf825fbd10421b
Felix Augustsson <git.fexilus@gmail.com>
2020-12-02 11:10:32 +0100

    Generalized code to any number of cameras

--- examples/frame_asyncronity.py
+++ examples/frame_asyncronity.py
@@ -4,31 +4,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-camera0 = h5py.File("data/data_109.h5", 'r')
-camera1 = h5py.File("data/data_130.h5", 'r')
+data_files = ["data/data_109.h5",
+              "data/data_130.h5",
+              "data/data_142.h5",
+              "data/data_143.h5"]
 
-min_num_frames = min(camera0["Timestamp"].size, camera1["Timestamp"].size)
+cameras = [h5py.File(data_file, 'r') for data_file in data_files]
 
-cropped_times0 = np.asarray(camera0["Timestamp"])[0:min_num_frames]
-cropped_times1 = np.asarray(camera1["Timestamp"])[0:min_num_frames]
+frame_counts = [camera["Timestamp"].size for camera in cameras]
+min_num_frames = min(frame_counts)
 
-time_step_frames0 = np.diff(np.asarray(camera0["Timestamp"]))
-time_step_frames1 = np.diff(np.asarray(camera1["Timestamp"]))
+cropped_timestamps = [np.asarray(camera["Timestamp"])[0:min_num_frames]
+                      for camera in cameras]
 
-average_step_size = np.median(np.concatenate((time_step_frames0,
-                                              time_step_frames1)))
+time_steps = [np.diff(np.asarray(camera["Timestamp"])) for camera in cameras]
 
-earliest_time = min(camera0["Timestamp"][0], camera1["Timestamp"][0])
+average_step_size = np.median(np.concatenate(time_steps))
+
+earliest_time = min([camera["Timestamp"][0] for camera in cameras])
 
 expected_end_time = average_step_size * min_num_frames
 expected_times = np.arange(0, expected_end_time, average_step_size)
 
-norm_frame_times0 = cropped_times0 - expected_times - earliest_time
-norm_frame_times1 = cropped_times1 - expected_times - earliest_time
+norm_frame_times = [cropped_timestamp - expected_times - earliest_time
+                    for cropped_timestamp in cropped_timestamps]
 
 plt.subplot()
-plt.plot(norm_frame_times0)
-plt.plot(norm_frame_times1)
+for norm_frame_time in norm_frame_times:
+    plt.plot(norm_frame_time)
 
 plt.xlabel("Frame number in dataset")
 plt.ylabel("Time difference compared to expected behavior (s)")
