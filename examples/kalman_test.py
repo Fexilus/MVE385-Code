c0c5812966b0dd9cb1d9adaed3ddba74864e5efb
Ebba Ekblom <ekebba@student.chalmers.se>
2020-12-10 11:23:06 +0100

    changed variable names do detections (fr. measurement)

--- examples/kalman_test.py
+++ examples/kalman_test.py
@@ -7,7 +7,7 @@ from tracking.visualize.predictions import visualize_predictions
 
 
 # Step one: make a Kalman filter for single sensor for single object
-datafile = "data/data_109.h5"
+datafile = "tracking/data/data_109.h5"
 camera = h5py.File(datafile, 'r')
 single_obj_ind = [2, 3, 3, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 2, 1, 1, 2, 1]
 single_obj_det = np.zeros((20,3)) 
@@ -21,6 +21,7 @@ for i in range(20):
 timestamps = camera["Timestamp"][0:20]
 time_steps = np.insert(np.diff(timestamps), 0, np.median(timestamps))
 
+print(time_steps)
 obj_track = basic.track(single_obj_det, time_steps)
 
 visualization_gen = zip(obj_track, range(20))
